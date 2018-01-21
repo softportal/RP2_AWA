4ad4d284d3cb22dedc2eb51c5a3ae29ce7929d0e
semedi <ssemedi@opennebula.org>
2018-01-21 01:07:02 +0100

    complete

--- tutorial/client-tutorial.c
+++ tutorial/client-tutorial.c
@@ -17,34 +17,60 @@ static void DefineHeaterObject(AwaClientSession * session)
 static void SetInitialValues(AwaClientSession * session)
 {
 	AwaClientSetOperation * operation = AwaClientSetOperation_New(session);
+    //instance
 	AwaClientSetOperation_CreateObjectInstance(operation, "/1000/0");
+
+    //manufacturer instance
 	AwaClientSetOperation_CreateOptionalResource(operation, "/1000/0/101");
 	AwaClientSetOperation_AddValueAsCString(operation, "/1000/0/101", "HotAir Systems Inc");
+
+    //temperature instance
     AwaClientSetOperation_CreateOptionalResource(operation, "/1000/0/104");
+    AwaClientSetOperation_AddValueAsFloat (operation, "/1000/0/104", 19.013);
+
 	AwaClientSetOperation_Perform(operation, OPERATION_PERFORM_TIMEOUT);
+    //perform op
 	AwaClientSetOperation_Free(&operation);
 }
 
-static void process_temp(float temp)
+static void process_temp(AwaFloat temp, AwaClientSession * session)
 {
-    printf("has escrito %f parguelon\n", temp);
+
+	AwaClientSetOperation * operation = AwaClientSetOperation_New(session);
+    int result;
+    printf("set %20lf temp \n", temp);
+
+	if (AwaClientSetOperation_AddValueAsFloat(operation, "/1000/0/104", temp) != AwaError_Success)
+	{
+		result = -1;
+	}
+
+	AwaClientSetOperation_Perform(operation, OPERATION_PERFORM_TIMEOUT);
+
+	AwaClientSetOperation_Free(&operation);
+
 }
 
 
 
 int main(void)
 {
-    float n1;
+    AwaFloat n1;
+	AwaFloat tmp = 0.0;
 	AwaClientSession * session = AwaClientSession_New();
 	AwaClientSession_Connect(session);
 	DefineHeaterObject(session);
+
+
 	SetInitialValues(session);
 
     while (true)
     {
+
+
         printf("enter temperature value ");
-        scanf("%f", &n1);
-        process_temp(n1);
+        scanf("%20lf", &tmp);
+        process_temp(tmp, session);
     }
 
 
